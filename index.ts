8556365e8a1c31e24f71aa71d3678841898ee353
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-21 23:59:21 -0400

    feat(global): use task routes

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
+import task from "./Task/task.network.ts";
+
 const app = new Application();
+
+app.use(task.routes());
+app.use(task.allowedMethods());
 
 await app.listen({ port: 8000 });

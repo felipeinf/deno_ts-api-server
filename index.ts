f26bd77a576bf1020f3d8e1a3483c6b6ac81caf8
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-22 23:26:59 -0400

    feat(global): mongo connect

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import mongo from "./Core/repository/mongo.module.ts";
 
 import task from "./Task/task.network.ts";
 
 const app = new Application();
 
+mongo.connect();
+
 app.use(task.routes());
 app.use(task.allowedMethods());
 
-await app.listen({ port: 8000 });
+await app.listen({ port: 35001 });

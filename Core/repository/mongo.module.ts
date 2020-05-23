afb101d919ac4f17f974de1b52e21ae53cda62b3
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-22 23:25:08 -0400

    feat(modules): find function in mongo module

--- Core/repository/mongo.module.ts
+++ Core/repository/mongo.module.ts
@@ -1,13 +1,22 @@
-import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+import { MongoClient, Database } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
 
 
-const client = new MongoClient();
+const client: MongoClient = new MongoClient();
+const database: Database = client.database(`${Deno.env.get('MONGO_DATABASE_NAME')}`)
 
-async function connect() {
-  const uri: string = Deno.env.get('MONGO_URI') || '';
-  console.log(uri);
-  client.connectWithUri(uri);
+function connect(): void {
+  client.connectWithUri(`${Deno.env.get('MONGO_URI')}`);
+}
+
+async function find<type>(collection: string, filter?: any) {
+  const result: any[] = await database.collection(collection)
+    .find(filter);
+
+  return result.map((value: any) => {
+    value._id = value._id.$oid;
+    return value as type;
+  });
 }
 
 export const db = client.database("blyke");

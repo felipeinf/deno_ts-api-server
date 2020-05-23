4168b24b5e93cd234aafd5e0a6ec10ec53c87d90
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-23 10:57:11 -0400

    feat(user): get user in repository

--- User/user.repository.ts
+++ User/user.repository.ts
@@ -5,3 +5,8 @@ import mongo from "./../Core/repository/mongo.module.ts";
 function getAllUser() {
   return mongo.find('user');
 }
+
+function getUser(id: string) {
+  return mongo.findOne('user', {_id: {$oid: id}});
+}
+

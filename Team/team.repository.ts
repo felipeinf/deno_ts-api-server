6fadb581c3dfe47322d3bb043d98cc7271e98dff
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-26 23:40:33 -0400

    feat(team): get all teams in repository

--- Team/team.repository.ts
+++ Team/team.repository.ts
@@ -1,5 +1,8 @@
 import { Team } from "./team.model.ts";
 import mongo from "./../Core/repository/mongo.module.ts";
 
+function getAllTeams() {
+  return mongo.find('team');
+}
 
 export default {  };
